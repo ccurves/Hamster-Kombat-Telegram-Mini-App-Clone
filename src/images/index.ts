fc405cdee2b04f4d12483775446d561640f84cbf
Covenant Simon <70116851+ccurves@users.noreply.github.com>
2024-07-01 22:19:43 +0100

    Update UI

--- src/images/index.ts
+++ src/images/index.ts
@@ -4,16 +4,16 @@ import dollarCoin from "./dollar-coin.png";
 import dailyReward from "./daily-reward.png";
 import dailyCipher from "./daily-cipher.png";
 import dailyCombo from "./daily-combo.png";
-import mainCharacter from "./main-character.png";
+import mainCharacter from "./main-character-2.png";
 import hamsterCoin from "./hamster-coin.png";
 
 export {
-    hamsterExchange,
-    binanceLogo,
-    dollarCoin,
-    dailyReward,
-    dailyCipher,
-    dailyCombo,
-    mainCharacter,
-    hamsterCoin
+  hamsterExchange,
+  binanceLogo,
+  dollarCoin,
+  dailyReward,
+  dailyCipher,
+  dailyCombo,
+  mainCharacter,
+  hamsterCoin,
 };
